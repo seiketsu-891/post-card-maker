aef24115f16bf3d54ed7748391b27224772d46e2
louie <ouseiketsu@protonmail.com>
2023-01-29 22:56:25 +0800

    feat: 添加可折叠资源选择视窗;实现画布区域基本布局;

--- front/postcard-maker/src/views/UserHomeView.vue
+++ front/postcard-maker/src/views/UserHomeView.vue
@@ -67,10 +67,24 @@
         </a-menu>
       </a-layout-sider>
       <!-- 主要内容区 -->
-      <a-layout-content class="main">main content</a-layout-content>
+      <a-layout-content class="main">
+        <!-- 资源选择区域（可折叠） -->
+        <div class="resource" v-show="reesourceAreaVisible">
+          resource
+          <!-- 折叠按钮 -->
+          <button class="resource__collapsebtn" @click="collapeResourceArea">
+            <LeftOutlined />
+          </button>
+        </div>
+        <!-- 明信片编辑区 -->
+        <div class="editor">
+          <div class="canvas-container"><div class="canvas">canvas</div></div>
+        </div></a-layout-content
+      >
     </a-layout>
   </div>
 </template>
+
 <script>
 import {
   FontSizeOutlined,
@@ -80,6 +94,7 @@ import {
   CopyOutlined,
   CrownFilled,
   CloudDownloadOutlined,
+  LeftOutlined,
 } from "@ant-design/icons-vue";
 export default {
   components: {
@@ -90,6 +105,17 @@ export default {
     CopyOutlined,
     CrownFilled,
     CloudDownloadOutlined,
+    LeftOutlined,
+  },
+  data() {
+    return {
+      reesourceAreaVisible: true,
+    };
+  },
+  methods: {
+    collapeResourceArea() {
+      this.reesourceAreaVisible = false;
+    },
   },
 };
 </script>
@@ -147,10 +173,55 @@ export default {
     margin-left: 120px
     height: calc(100vh - 60px)
     background-color:#EBECF0
+    display: flex
+    .resource
+      flex-shrink: 0
+      width: 300px
+      height: 100%
+      background-color: #fff
+      // background-color: #343536
+      position: relative
+      &__collapsebtn
+        // background-color: #343536
+        background-color: #fff
+        position: absolute
+        right: -30px
+        top: 50%
+        margin-top: -50px
+        width: 30px
+        height: 100px
+        border: 0
+        border-radius: 0 20px 20px 0
+        text-align: center
+        cursor: pointer
+        &:hover
+          color: #4661F4
+    .editor
+      width: 100%
+      height: 100%
+      overflow: scroll
+      padding: 80px
+      // 这里如果在editor中直接设置flex的话，会导致垂直方向padding无法正常显示，因此需要给canvas再添加一个父容器
+      .canvas-container
+        min-width: 100%
+        min-height: 100%
+        display: flex
+        align-items: center
+        justify-content: center
+        // 如果不添加float，当水平方向overflow时，右边的padding会消失。
+        float: left
+      .canvas
+        flex-shrink: 0
+        width: 900px
+        height: 900px
+        background: #fff
+        box-shadow: rgba(33, 35, 38, 0.2) 0px 10px 10px -10px
+
 .side
     top:60px
     position: fixed
     &__menu
+      // background: #252627 !important
       padding: 10px
       height: 100vh
       width: 120px
